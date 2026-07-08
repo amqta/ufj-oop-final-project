051ee31a707c14cf2b9472d020abb74f01f590fc
Arthur Mesquita <nunesarthurr@gmail.com>
2026-07-08 18:17:32 -0300

    feat: now using the Json DAO

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,9 +1,10 @@
 package model.dao;
 
 import model.dao.impl.ExemplarDaoFiles;
+import model.dao.impl.ExemplarDaoJson;
 
 public class DaoFactory {
     public static ExemplarDao createExemplarDao() {
-        return new ExemplarDaoFiles();
+        return new ExemplarDaoJson();
     }
 }
